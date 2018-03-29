ebef72d7b0e6a210de4e79b176cd92570279a74b
m305-62\w401 <grrr@fjgioe444.fii>
2018-03-29 17:37:49 +0800

    停留在女身高平均

---
+++ 列名/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] name = { "葉俊廷", "張澤瑜", "王程捷 的咩？", "江儀婷", "張秉華", "陳柏霖", "黃紀瑜", "黃昱維", "蔡逸群", "登琳", "世新魏鈞孝吧", "劉定南", "陳信如", "童信傑", "李岳倫", "鄒和恆", "簡毓玟", "劉子瑄", "蕭紹洋", "李亞宸la", "張以潔", "邱仕紳", "呂家瑩", "世新林鑫佑", "徐均得", "陳佳欣", "張恩瑋", "周詮", "王湘婷", "蕭宇成", "Max Cheung(BANG)", "馬嘉誠Σヽ(ﾟД ﾟ; )ﾉ魔貫光殺砲(ﾟДﾟ)σ━0000", "陳昱嘉", "康珈熏", "盧奕宏", "陳郁佳的大哥黃博涵", "遲正雯", "世新林星彤", "許晏誠ｘ勒是奇多＿８＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝Ｄ　", "許子安", "高子晴", "陳思婷", "矝鵘鮿", "余彥廷", "吳耀輝", "鄭丞智", "林冠廷", "世新何曜宇", "邵喬雨", "張豐愷", "林子晴", "邱雁回", "陳家欣是韓妞", "鄧雅馨", "謝汶珊", "李曼寧", "世新徐偉哲", "张钰慈", "鍾宜珊", "raer_tsai", "世新李姳諼", "鄭曼君", "陳柏霖", "世新許子安", "世新方若帆", "李曼寧", "世新遲正雯", "世新周詮" };
+            //string[] BG = { "男", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "男", "女", "男", "男", "男", "女", "女", "男", "男", "女", "男", "女", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "女", "男", "男", "女", "女", "男", "男", "女", "女", "男", "男", "男", "男", "男", "男", "女", "男", "女", "女", "女", "女", "女", "女", "男", "女", "女", "女", "女", "女", "男", "男", "女", "女", "女", "男" };
+
+            // 設置血型資
+            string[] blood = { "O", "其他", "O", "O", "A", "O", "A", "A", "O", "O", "A", "O", "A", "B", "O", "O", "A", "其他", "O", "O", "A", "", "B", "O", "", "O", "B", "O", "B", "B", "B", "O", "O", "AB", "O", "B", "A", "O", "O", "", "O", "A", "", "O", "O", "A", "O", "O", "其他", "B", "O", "O", "O", "A", "AB", "A", "O", "B", "AB", "", "O", "O", "O", "", "O", "A", "A", "O" };
+
+            // 計算各血型人數
+            int countA = 0;
+            int countB = 0;
+            int countO = 0;
+            int countAB = 0;
+
+            // 篩選
+            for (int r = 0; r < blood.Length; r++)
+            {
+                // Console.WriteLine( r + ":" + BG[r] + "-"  + blood[r] + "-" + name [r] );
+
+                
+                if (blood [r] == "A")
+                {
+                    countA ++;
+                }
+                if (blood[r] == "B")
+                {
+                    countB++;
+                }
+                if (blood[r] == "O")
+                {
+                    countO++;
+                }
+                if (blood[r] == "AB")
+                {
+                    countAB++;
+                }
+            }
+
+            // 運算所占百分比
+            double percentA = (double)countA / (double)blood.Length * 100;
+            double percentB = (double)countB / (double)blood.Length * 100;
+            double percentO = (double)countO / (double)blood.Length * 100;
+            double percentAB = (double)countAB / (double)blood.Length * 100;
+
+            // 列印
+            Console.WriteLine( "A型人數:"  + countA , percentA );
+            Console.WriteLine( "B型人數:" + countB + "-占全班的:" + percentB + "%");
+            Console.WriteLine( "O型人數:" + countO + "-占全班的:" + percentO + "%");
+            Console.WriteLine( "AB型人數:" + countAB + "-占全班的:" + percentAB + "%");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 列名/血型/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] genders = { "男", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "男", "女", "男", "男", "男", "女", "女", "男", "男", "女", "男", "女", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "女", "男", "男", "女", "女", "男", "男", "女", "女", "男", "男", "男", "男", "男", "男", "女", "男", "女", "女", "女", "女", "女", "女", "男", "女", "女", "女", "女", "女", "男", "男", "女", "女", "女", "男" };
+            int[] height = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 167, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 165, 155, 0, 168, 160, 169, 0, 150, 163, 168, 173 };
+
+            int sum = 0;
+            int count = 0;
+
+            for(int i =0; i< height.Length; i++ )
+            {
+                if (height[i] > 100 && height[i] < 200)
+                {
+                    if( genders [i] = "女")
+                    {
+                        sum = sum + height[i];
+                        count++;
+                    }
+                }
+            }
+
+
+            Console.WriteLine("女生身高總合:" + sum);
+            Console.WriteLine("女生身高平均:" + sum / count);
+            Console.ReadKey;
+        }
+    }
+}
